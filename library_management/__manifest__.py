2339f14d9f2fb4a42434bb7b41f62796a5100c52
Bilal-Yoosuf <bilalyoosuf22@gmail.com>
2020-12-16 15:28:53 +0530

    first commit

---
+++ library_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Library Management",
+    'version': '13.1',
+    'depends': ['base'],
+    'author': "Socius Trainee",
+    'category': 'Category',
+    'description': "Library management module",
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+    'installable': True
+
+}
---
+++ student_management/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Student Manageemnt",
+    'version': '13.1.0',
+    'depends': ['base'],
+    'author': "Socius Trainee",
+    'category': 'Category',
+    'description': "Student Management module",
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+    'installable': True
+}
