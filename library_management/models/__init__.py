8019ed4f18e3f998e17889ccbacd86954d78f99b
jithinbabu296 <jithinbabuthaiparambil@gmail.com>
2020-12-21 12:23:09 +0530

    change class name and code standardisation

--- library_management/models/__init__.py
+++ library_management/models/__init__.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
+
 
 from . import book_return
-=======
-
-# -*- coding: utf-8 -*-
-
-from . import book_registration
-from . import models
-
->>>>>>> ddec18ddfd0e9fe449787ec429c7506317f6c289
+from .import book_registration
